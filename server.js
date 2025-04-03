1ed4e5f555d467d8e5a93c364b57fa13ef2e1ae9
Your GitHub Username <audreygride@gmail.com>
2025-04-03 03:47:17 +0300

    second commit

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-import WebSocket from 'ws';
-import http from 'http';
+const WebSocket = require('ws');
+const http = require('http');
 
-// Use the provided PORT or default to 8080
 const PORT = process.env.PORT || 8080;
-
-// Create an HTTP server (Render requires an HTTP listener)
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('WebSocket Server is running!\n');
 });
 
-// Attach WebSocket server to HTTP server
 const wss = new WebSocket.Server({ server });
 
 console.log(`WebSocket server running on port ${PORT}`);
@@ -34,7 +30,6 @@ wss.on('connection', (socket) => {
     });
 });
 
-// Start the HTTP server
 server.listen(PORT, () => {
     console.log(`HTTP Server running on port ${PORT}`);
 });
